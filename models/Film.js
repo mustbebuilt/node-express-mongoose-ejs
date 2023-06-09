a6173bd217f75d47e5b1cf749448eacc33fd4c4d
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-06-09 08:36:55 +0100

    Consist use of FilmTitle

--- models/Film.js
+++ models/Film.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const filmSchema = new mongoose.Schema(
   {
     filmCertificate: String,
-    filmName: String,
+    filmTitle: String,
     filmDescription: String,
     filmImage: String,
     filmPrice: Number,
